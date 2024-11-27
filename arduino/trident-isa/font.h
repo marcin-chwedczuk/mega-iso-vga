d4bdc23ba332e465dd7d2a1213ba75d1c3b2f176
Marcin Chwedczuk <0xmarcin+gh@gmail.com>
2024-11-27 15:12:53 +0100

    remove unsued font

--- arduino/trident-isa/font.h
+++ arduino/trident-isa/font.h
@@ -1,23 +1,8 @@
 #ifndef FONT_H
 #define FONT_H
 
-#include <stdint.h>
-
-// Externs
+// List of defined fonts:
 
 extern  unsigned char Font8x16[];
-extern  unsigned char Font8x8[];
-
-// Prototypes
-
-void SetFont8x16(void);
-
-void SetFont8x8(void);
-
-void  FontsRead( uint8_t  *biosfont,uint8_t uint8_tsperchar );
-
-uint8_t VgaIoReadIx(uint32_t addr, uint8_t ix);
-void VgaIoWriteIx(uint32_t addr, uint16_t valIx);
-void writeMemoryByte(uint32_t address, uint8_t data);
 
 #endif
